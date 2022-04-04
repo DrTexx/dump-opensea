f7fa1b9f2639e88aaa3c74ffdc4fc83a4eceadb1
DrTexx <23412100+DrTexx@users.noreply.github.com>
2022-04-05 02:52:25 +1000

    Support OpenSea API Keys and rearchitect script
    - Added support for OpenSea API Keys
    - Automatically collect total minted from OpenSea API
    - Removed Total Minted argument (no longer required)
    - Abstracted dict-based choice prompt into it's own generalised function
    - Add statusMsgs for progress context
    - Suppress expected errors during _getOwners() function loop
    - Refactor logic for collecting token_ids into CollectTokenIds methods
    - Re-enabled OpenSea support
    - Improve general readability and documentation
    - Improve error-handling
    - Left another comment down here to let you if you're reading this I love you <3

--- dump-opensea.py
+++ dump-opensea.py
@@ -47,7 +47,8 @@ def showHeader():
               ░░░░░                      - BROUGHT TO YOU BY DOS PUNKS DAO                         
 
                 {Fore.WHITE}Created by: {Fore.LIGHTRED_EX}GBE#0001{Fore.WHITE} for DOS PUNKS DAO
-                {Fore.WHITE}Updated by: {Fore.LIGHTRED_EX}DENVERS.ETH{Fore.WHITE} - Error context, support for JSON imports, alternative user input handling, refactoring
+             {Fore.WHITE}Reimagined by: {Fore.LIGHTRED_EX}DENVERS.ETH{Fore.WHITE} - Error context, support for JSON imports, parser rearchitecture, refactoring,
+                                          ... support for OpenSea API keys.
     """
     )
 
@@ -63,31 +64,91 @@ def inputStyled(argumentName):
     return input().strip()
 
 
+def _keys_to_string(_dict) -> str:
+    """Return a comma-separated list of keys for a given object.
+
+    Should work with anything that can be used as a dict key (e.g. string, number, etc.)"""
+    return ",".join([str(key) for key in _dict.keys()])
+
+
+# TODO: improve docstring
+# TODO: move to own script/library
+def chooseFromDict(
+    _dict,
+    message_prompt,
+    value_prompt_attr,
+    message_invalid=f"{Fore.RED}\nSorry, that's not a valid choice. Please try again.\n{Style.RESET_ALL}",
+    display_options=True,
+    convert_ints=True,
+    retry=False,
+):
+    """Prompt user to select an option from a given dictionaries.
+
+    Returns the value of the corrosponding key choice.
+
+    - value_prompt_attr: an attribute each value in the dictionary has which is
+        - Used as context for the value when displaying choices
+    """
+    # Collect options based on dict keys
+    options = {i: a for (i, a) in enumerate(_dict)}
+
+    # Generate prompt message
+    prompt = f"{message_prompt} [{_keys_to_string(options)}]"
+
+    while True:
+        # Display options to user
+        if display_options:
+            for (i, a) in options.items():
+                option_context = getattr(_dict[i], value_prompt_attr)
+                print(f" {i}: {option_context}")
+            print()
+
+        # Prompt for choice
+        sourceChoice = inputStyled(prompt)
+
+        # Convert integers if applicable
+        if convert_ints:
+            if sourceChoice.isdigit():
+                sourceChoice = int(sourceChoice)
+
+        # If choice is valid, return corrosponding dict value
+        if sourceChoice in options.keys():
+            return options[sourceChoice]
+        else:
+            print(message_invalid)
+            if not retry:
+                raise ValueError("Invalid choice")
+
+
 # Init with flags
 def flagInit():
     global userWasPrompted
     # Parse Flags
     parser = argparse.ArgumentParser()
-    tokenIdSource = parser.add_mutually_exclusive_group()
+    requiredParameters = parser.add_argument_group("required")
+    tokenIdSource = parser.add_argument_group(
+        "sources"
+    ).add_mutually_exclusive_group()
     advancedParameters = parser.add_argument_group("advanced")
+    openseaParameters = parser.add_argument_group("opensea")
     actions = [
+        # Required arguments
+        requiredParameters.add_argument(
+            "-c", "--contract", help="Contract Address", default="==PROMPT=="
+        ),
+        requiredParameters.add_argument(
+            "-k", "--apikey", help="API Key (Moralis)", default="==PROMPT=="
+        ),
+        # Required mutex arguments
         tokenIdSource.add_argument("-s", "--slug", help="Slug Name (OpenSea)"),
         tokenIdSource.add_argument(
             "-t", "--token-id-json", help="Token ID JSON"
         ),
-        parser.add_argument(
-            "-c", "--contract", help="Contract Address", default="==PROMPT=="
-        ),
-        parser.add_argument(
-            "-m", "--minted", help="Total Minted", default="==PROMPT=="
-        ),
-        parser.add_argument(
-            "-k", "--apikey", help="API Key", default="==PROMPT=="
-        ),
+        # Optional arguments
         parser.add_argument(
             "-f",
             "--filter",
-            help="Filter by Tokens (2 If you want to filter by holders with 2 or more tokens)",
+            help="Filter Owners (Minimum Tokens Held)",
             type=int,
         ),
         advancedParameters.add_argument(
@@ -96,6 +157,10 @@ def flagInit():
             help="Show error stacktraces",
             action="store_true",
         ),
+        # OpenSea specific
+        openseaParameters.add_argument(
+            "-o", "--opensea-api-key", help="API Key (OpenSea)"
+        ),
     ]
 
     try:
@@ -103,59 +168,70 @@ def flagInit():
 
         # Prompts begin
 
-        # Ensure information for token ID source
-        if not (args.slug or args.token_id_json):
-            # NOTE: unsure if _group_actions are scoped to this specific mutex group or parser itself
-            for i, a in enumerate(tokenIdSource._group_actions):
-                print(f"{i} - {a.help}")
-            tokenIdSource = int(
-                inputStyled(
-                    "Which token_id source would you like to use? [0,1]"
-                )
-            )
-            # HACK: using indices of actions here is prone to break if arg order changes
-            slugAction = actions[0]
-            jsonAction = actions[1]
-            if tokenIdSource == 0:
-                values = inputStyled(slugAction.help)
-                setattr(args, slugAction.dest, values)
-            elif tokenIdSource == 1:
-                values = inputStyled(jsonAction.help)
-                setattr(args, jsonAction.dest, values)
-            else:
-                raise ValueError("Invalid selection")
+        # Create dictionary of dest-indexed actions
+        destActions = {
+            action.dest: action
+            for action in actions
+            if hasattr(action, "dest")
+        }
 
+        # Ensure source for token IDs provided
+        if not (args.slug or args.token_id_json):
+            # Prompt user to select a source
+            sourceAction = chooseFromDict(
+                tokenIdSource._group_actions,
+                "Which token_id source would you like to use?",
+                "help",
+                )
+
+            # Set source-specific argument to be prompted
+            setattr(args, sourceAction.dest, "==PROMPT-STRICT==")
+
+            statusMsg("SELECTED TOKEN ID SOURCE.")
+
+            # Note that user was prompted
             userWasPrompted = True
+
+        # Ensure OpenSea args are set (if necessary)
+        if args.slug:
+            if not args.opensea_api_key:
+                setattr(
+                    args,
+                    destActions["opensea_api_key"].dest,
+                    "==PROMPT-STRICT==",
+                )
+
+        # Prompt user for values for all arguments set to "==PROMPT=="
         for a in actions:
-            if getattr(args, a.dest) == "==PROMPT==":
+            strict = getattr(args, a.dest) is "==PROMPT-STRICT=="
+            if getattr(args, a.dest) in ("==PROMPT==", "==PROMPT-STRICT=="):
                 values = inputStyled(a.help)
                 setattr(args, a.dest, values)
                 userWasPrompted = True
+                # If prompt-strict, raise an error if the provided value is falsy
+                if strict:
+                    if bool(getattr(args, a.dest)) is False:
+                        raise Exception(f"{a.dest} mustn't be falsy")
+    except KeyboardInterrupt:
+        raise Exception("Quitting...")
     except Exception as err:
         raise Exception(f"An error occuring during parsing: {err}")
 
     # Get flags
     (
+        contract,
+        apiKey,
         slug,
         tokenIdJson,
-        contract,
-        totalMinted,
-        apiKey,
         tokenFilter,
         excepTraces,
+        openseaApiKey,
     ) = vars(args).values()
 
     # Enable exception traces
     if excepTraces is True:
         global exception_traces
         exception_traces = True
-
-    # Ensure totalMinted is an int
-    # (argparse 'type' kwarg not used instead because it breaks ==PROMPT== functionality)
-    try:
-        totalMinted = int(totalMinted)
-    except ValueError as err:
-        raise ValueError("Total Minted must be an integer (whole number)")
 
     # Check either slug or tokenIdJson are set
     if not (slug or tokenIdJson):
@@ -172,19 +248,20 @@ def flagInit():
             )
 
     if userWasPrompted is True:
-        print(
-            f"\n{Fore.YELLOW}{Back.BLACK} ALL REQUIRED PARAMETERS COLLECTED. {Style.RESET_ALL}\n"
-        )
+        statusMsg("ALL REQUIRED PARAMETERS COLLECTED.")
 
+    # TODO: add any missing flags
     print(
         f"[] Slug: {styleArgVal(slug)}\n"
         + f"[] Token ID JSON: {styleArgVal(tokenIdJson)}\n"
         + f"[] Contract Address: {styleArgVal(contract)}\n"
-        + f"[] Total Minted: {styleArgVal(totalMinted)}\n"
-        + f"[] API Key: {styleArgVal(apiKey)}\n"
-        + f"[] Filter: {styleArgVal(tokenFilter)}\n"
+        + f"[] API Key (Moralis): {styleArgVal(apiKey)}\n"
+        + f"[] API Key (OpenSea): {styleArgVal(openseaApiKey)}\n"
+        + f"[] Filter: {styleArgVal(tokenFilter)}",
     )
-    return slug, tokenIdJson, contract, totalMinted, apiKey, tokenFilter
+
+    # TODO: add any missing flags
+    return slug, tokenIdJson, contract, apiKey, tokenFilter, openseaApiKey
 
 
 # Fatal Error --> Exit
@@ -227,6 +304,24 @@ class API:
             headers=headers,
         )
 
+    # TODO: add typehints to funcsig
+    def get_opensea_collection_stats(collection_slug, opensea_api_key):
+        """Get stats on an OpenSea collection with the collection's slug."""
+        headers = {}
+        if opensea_api_key is not None:
+            headers["X-API-Key"] = opensea_api_key
+
+        response = requests.get(
+            f"https://api.opensea.io/api/v1/collection/{collection_slug}",
+            headers=headers,
+        )
+        if not response.ok:
+            raise RuntimeError(
+                f"Failed to get OpenSea collection stats: {response.status_code} ({response.reason})"
+            )
+
+        return response.json()["collection"]["stats"]
+
 
 def _getOwners(tokenIds, contract, apiKey, bar):
     for tokenId in tokenIds:
@@ -252,19 +347,19 @@ def _getOwners(tokenIds, contract, apiKey, bar):
                 if not "total" in web3response:
                     raise RuntimeError("total is missing!")
                 break
+            # TODO: Add debug flag that re-enables error printing in this section
             except RuntimeError as err:
-                print(err)
-                print(
-                    "might be getting throttled, trying again in 1.5 seconds..."
-                )
+                # print(err)
+                # print("might be getting throttled, trying again in 1.5 seconds...")
                 time.sleep(1.5)
             except KeyboardInterrupt:
-                print("Cancelled by user...")
+                # print("Cancelled by user...")
                 raise KeyboardInterrupt
             except requests.exceptions.ReadTimeout:
-                print(
-                    f"{Fore.YELLOW}Timed-out waiting for server response...{Style.RESET_ALL}"
-                )
+                continue
+                # print(
+                #     f"{Fore.YELLOW}Timed-out waiting for server response...{Style.RESET_ALL}"
+                # )
             except Exception as err:
                 print(response.status_code, response.reason, type(err))
                 print("Unexpected error, trying again in 1 second...")
@@ -282,63 +377,13 @@ def _getOwners(tokenIds, contract, apiKey, bar):
 
 
 # Retrieving owners from API's
-def getOwners(slug, tokenIdJson, contract, pagination, tokenFilter, apiKey):
-    # Iterate through the pagination
-    print(
-        f"""\n{Fore.YELLOW}
-        +------------------------------------------------------------+
-        |              CHECKING HOLDERS. PLEASE WAIT...              |
-        +------------------------------------------------------------+{Fore.WHITE}\n"""
-    )
-    try:
-        # if slug is specified, ignore it (argparse should've already performed conflict check)
-        if tokenIdJson:
-            # JSON IMPORT METHOD
-            with open(tokenIdJson) as f:
-                dict_root = json.load(f)
-                tokenIds = dict_root["tokenIds"]
+def getOwners(contract, tokenIds, tokenFilter, apiKey):
+    # showCheckingHolders()
+
                 with progressBar(len(tokenIds), bar="filling") as bar:
                     _getOwners(tokenIds, contract, apiKey, bar)
-        else:
-            # SLUG BASED METHOD
 
-            # FIXME: pagniation broken (probably easiest to just refactor)
-            raise NotImplementedError(
-                "Sourcing token IDs from OpenSea collection slug is temporarily disabled"
-            )
-
-            # TODO: Requires testing by another person with an OpenSea API key
-            for i in range(0, int(pagination)):
-                offset = i * 50
-
-                # TODO: Add support for OpenSea API keys
-                # FIXME: TEMP
-                # opensea_api_key = ""
-
-                # Fetch OpenSea collection assets
-                response = API.get_opensea_collection_assets(slug, offset, "")
-                if response.ok is not True:
-                    raise RuntimeError(
-                        f"Failed to collect assets from OpenSea API: {response.status_code} ({response.reason})"
-                    )
-
-                # Parse OpenSea response
                 try:
-                    # NOTE: .assets[n].owner.address is always 0x000...0 (guess: related to frozen/non-frozen metadata?)
-                    responseJson = response.json()
-                except Exception as err:
-                    raise RuntimeError(
-                        f"Failed to parse OpenSea response: {err}"
-                    )
-
-                # Extract token_ids from response
-                tokenIds = [
-                    asset["token_id"] for asset in responseJson["assets"]
-                ]
-
-                with progressBar(int(pagination), bar="filling") as bar:
-                    _getOwners(tokenIds, contract, apiKey, bar)
-
         if tokenFilter not in (None, 0):
             print(
                 f"\n\n{Fore.WHITE}[+] Filtered by holders with: {Fore.GREEN}{tokenFilter} or + Tokens{Fore.WHITE}"
@@ -354,6 +399,7 @@ def getOwners(slug, tokenIdJson, contract, pagination, tokenFilter, apiKey):
 # Export to JSON
 def exportJSON(slug):
     global totalNfts, totalOwned, owners
+    # TODO: Add timestamp to snapshot file
     outputFile = open(f"./snapshots/{slug}-owners.json", "w")
     json.dump(owners, outputFile)
     time.sleep(1.5)
@@ -369,27 +415,111 @@ def exportJSON(slug):
     )
 
 
+class CollectTokenIds:
+    def from_opensea(slug, contract, opensea_api_key):
+        # Get size of collection
+        collection_stats = API.get_opensea_collection_stats(
+            slug, opensea_api_key
+        )
+        # NOTE: Supersedes totalMinted argument
+        collection_size = collection_stats["total_supply"]
+
+        # Figure out how many requests we need to make
+        assets_per_request = 50
+        requests_left = math.ceil(collection_size / assets_per_request)
+        collected = 0
+        token_ids = []
+
+        with progressBar(requests_left, bar="filling") as bar:
+            for r in range(requests_left):
+                # Calculate request's offset
+                offset = r * assets_per_request
+
+                # Collect OpenSea collection assets
+                response = API.get_opensea_collection_assets(
+                    slug, offset, opensea_api_key
+                )
+                if response.ok is not True:
+                    raise RuntimeError(
+                        f"Failed to collect assets from OpenSea API: {response.status_code} ({response.reason})"
+                    )
+
+                # Parse OpenSea response
+                try:
+                    # NOTE: .assets[n].owner.address is always 0x000...0 (guess: related to frozen/non-frozen metadata?)
+                    responseJson = response.json()
+                except Exception as err:
+                    raise RuntimeError(
+                        f"Failed to parse OpenSea response: {err}"
+                    )
+
+                # Extract token_ids from response, append to total collection
+                token_ids.extend(
+                    [asset["token_id"] for asset in responseJson["assets"]]
+                )
+
+                # Increment collected counter
+                collected += assets_per_request
+                if collected > collection_size:
+                    collected = collection_size
+
+                # Announce progress
+                # print(f"Request #{r} - {collected} assets collected")
+                bar()
+
+        return token_ids
+
+    def from_json(tokenIdJson):
+        with open(tokenIdJson) as f:
+            return json.load(f)["tokenIds"]
+
+
+def statusMsg(msg):
+    """Prints status message with common formatting."""
+    print(f"\n{Fore.YELLOW}{Back.BLACK} {msg} {Style.RESET_ALL}\n")
+
+
 # Main handle
 def main():
     coloramaInit()
     showHeader()
     try:
+        # Gather arguments
         (
             slug,
             tokenIdJson,
             contract,
-            totalMinted,
             apiKey,
             tokenFilter,
-        ) = flagInit()  # Init with flags
+            openseaApiKey,
+        ) = flagInit()
+
+        statusMsg("COLLECTING TOKEN IDS.")
+
+        # Gather token_ids
+        if slug:
+            tokenIds = CollectTokenIds.from_opensea(
+                slug, contract, openseaApiKey
+            )
+        elif tokenIdJson:
+            tokenIds = CollectTokenIds.from_json(tokenIdJson)
+        else:
+            raise Exception(
+                "No valid token_ids sources to selected! - Please open a GitHub issue if you see this"
+            )
+
+        statusMsg("COLLECTING OWNERS.")
+
+        # Get owners
+        getOwners(contract, tokenIds, tokenFilter, apiKey)
+
+        statusMsg("EXPORTING RESULTS.")
+
+        # Export results
+        exportJSON(slug)
+
     except Exception as err:
         fatalError(err)
-
-    pagination = int(math.ceil((int(totalMinted)) / 50))  # Get pagination
-    getOwners(
-        slug, tokenIdJson, contract, pagination, tokenFilter, apiKey
-    )  # Main method
-    exportJSON(slug)  # Export results
 
 
 # Start
